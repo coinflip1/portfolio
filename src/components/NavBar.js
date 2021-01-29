58d18973de9a5a0d2d1f09397ab0b1b98c228fea
coinflip1 <73563987+coin@users.noreply.github.com>
2021-01-29 16:41:35 -0500

    magic scroll

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,18 +3,46 @@ import './NavBar.css';
 
 const NavBar=()=> {
     return (
+        <body>
         <div className='container'>
             <nav className='navbar'>
 <ul>
     <li><a href='#home'>Home</a></li>
     <li><a href='#about'>About</a></li>
     <li><a href='#projects'>Projects</a></li>
-    <li><a href='#skills'></a>Skills</li>
+    <li><a href='#skills'>Skills</a></li>
     <li><a href='#tools'>Tools</a></li>
     <li><a href='#contact'>Contact</a></li>
 </ul>
             </nav>
-        </div>
+
+        <section id='home'>
+            <h1>home of site</h1>
+            <p>lorem15lorem15lorem15lorem15lorem15lorem15lorem15lorem15</p>
+        </section>
+        <section id='about'>
+            <h1>about</h1>
+            <p>lorem15lorem15lorem15lorem15lorem15lorem15lorem15lorem15</p>
+        </section>
+         <section id='projects'>
+            <h1>project</h1>
+            <p>lorem15lorem15lorem15lorem15lorem15lorem15lorem15lorem15</p>
+        </section>
+        <section id='skills'>
+            <h1>skills</h1>
+            <p>lorem15lorem15lorem15lorem15lorem15lorem15lorem15lorem15</p>
+        </section>
+        <section id='tools'>
+            <h1>tools</h1>
+            <p>lorem15lorem15lorem15lorem15lorem15lorem15lorem15lorem15</p>
+        </section>
+        <section id='contact'>
+            <h1>contact</h1>
+            <p>lorem15lorem15lorem15lorem15lorem15lorem15lorem15lorem15</p>
+        </section>
+        
+        </div></body>
+
     )
 }
 
