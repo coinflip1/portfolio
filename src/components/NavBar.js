01b73209c846d8705e9d88e9f5fa8bf859a2a770
coinflip1 <73563987+coin@users.noreply.github.com>
2021-01-30 16:55:05 -0500

    change navbar
    change navbar from transparent

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './NavBar.css';
 
 const NavBar=()=> {
+  const [navbar,setNavbar]=useState(false);
+  const changeBackground=()=>{
+      if(window.scrollY>=80){
+          setNavbar(true);
+      }else{
+          setNavbar(false);
+      }
+    console.log(window.scrollY)
+  };
+  window.addEventListener('scroll',changeBackground);
     return (
         <body>
         <div className='container'>
-            <nav className='navbar'>
+            <nav className={navbar ? "navbar active" : "navbar"}>
+                <a  href='#'>
+                    <img className='nav__img' src='https://codesign.com.bd/conversations/content/images/2020/03/Sprint-logo-design-Codesign-agency.png'/>
+                </a>
 <ul>
     <li><a href='#home'>Home</a></li>
     <li><a href='#about'>About</a></li>
@@ -44,6 +57,15 @@ const NavBar=()=> {
         </div></body>
 
     )
+    
 }
-
+// var nav= document.querySelector('.navbar');
+// window.addEventListener('scroll',function(){
+//     if(window.pageYOffset>100){
+//         nav.classList += 'large';
+        
+//     }else{
+//         nav.classList='main-nav';
+//     }
+// })
 export default NavBar;
