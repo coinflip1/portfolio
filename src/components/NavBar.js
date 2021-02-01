23bd6e4616d9309091472c728d3283dd85ef416f
coinflip1 <73563987+coin@users.noreply.github.com>
2021-01-31 20:28:05 -0500

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,13 @@
 import React,{useState} from 'react';
 import './NavBar.css';
 
+
 const NavBar=()=> {
+    
   const [navbar,setNavbar]=useState(false);
+
+
+
   const changeBackground=()=>{
       if(window.scrollY>=80){
           setNavbar(true);
@@ -11,14 +16,16 @@ const NavBar=()=> {
       }
     console.log(window.scrollY)
   };
+  
   window.addEventListener('scroll',changeBackground);
+  
     return (
         <body>
-        <div className='container'>
+        <div className='container' id='contid' >
             <nav className={navbar ? "navbar active" : "navbar"}>
                 <a  href='#'>
-                    <img className='nav__img' src='https://codesign.com.bd/conversations/content/images/2020/03/Sprint-logo-design-Codesign-agency.png'/>
-                </a>
+                    <img className={navbar ? 'nav__img active' : 'nav__img'} src='https://codesign.com.bd/conversations/content/images/2020/03/Sprint-logo-design-Codesign-agency.png'/> 
+                    </a>
 <ul>
     <li><a href='#home'>Home</a></li>
     <li><a href='#about'>About</a></li>
@@ -59,6 +66,10 @@ const NavBar=()=> {
     )
     
 }
+
+document.getElementById('home').addEventListener('scroll',()=>console.log(document.scrolly));
+
+
 // var nav= document.querySelector('.navbar');
 // window.addEventListener('scroll',function(){
 //     if(window.pageYOffset>100){
